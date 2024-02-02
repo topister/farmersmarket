99f3939d50d7c2047b9c760e5ce4ffd51c392ccb
topister <topistern2@gmail.com>
2024-02-02 23:53:56 +0200

    customize Admin page using jazzmin

--- emarketproject/emarketproject/settings.py
+++ emarketproject/emarketproject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -130,3 +131,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS = {
+    'site_header': 'AgriEmpower Africa',
+    'site_logo': 'assets/imgs/logo/logo.png',
+    # 'site_brand': 'Growth beyond borders',
+    'site_brand': 'AgriEmpower Africa',
+    'site_copyright': 'agriempowerafrica.com/'
+}

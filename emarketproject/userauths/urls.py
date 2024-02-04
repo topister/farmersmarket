85c47e148d6127e12d28f6b0ecb24f22685614e8
topister <topistern2@gmail.com>
2024-02-04 12:12:36 +0200

    user sign in page

--- emarketproject/userauths/urls.py
+++ emarketproject/userauths/urls.py
@@ -4,5 +4,6 @@ from userauths import views
 app_name = "userauths"
 
 urlpatterns = [
-    path("signUp", views.registerView,  name="signUp"),
+    path("signUp/", views.registerView,  name="signUp"),
+    path("signIn/", views.loginView, name="signIn"),
 ]
--- emarketproject/userauths/views.py
+++ emarketproject/userauths/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import redirect, render
 from userauths.forms import SignUpForm
 from django.contrib import messages
 from django.contrib.auth import login, authenticate
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 # Create your views here.
 def registerView(request):
@@ -29,3 +32,32 @@ def registerView(request):
         'form': form,
     }
     return render(request, "userauths/signUp.html", context)
+
+def loginView(request):
+    if request.user.is_authenticated:
+        messages.warning(request, f"You are already logged in.")
+        return redirect("core:index")
+    
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.warning(request, f"User with  the email address {email} does not exist.")
+
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, f"You are logged in as {user.username}")
+            return redirect("core:index")
+        
+        else:
+            messages.warning(request, "User does not exist. Create new account!")
+    context = {
+
+    }
+        
+    return render(request, "userauths/signIn.html", context)
+

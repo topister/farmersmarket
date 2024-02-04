bd8f627c510fec3e4783d7478e9ac4cb59428ae9
topister <topistern2@gmail.com>
2024-02-04 11:08:01 +0200

    sign up page

--- emarketproject/userauths/forms.py
+++ emarketproject/userauths/forms.py
@@ -3,6 +3,11 @@ from django.contrib.auth.forms import UserCreationForm
 from userauths.models import User
 
 class SignUpForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Type your username..."}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": "Email Address"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "Password"}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"}))
+
     class Meta:
         model = User
         fields = ['username', 'email']

d42f5fc656c170669234da0ec1e5b49d78e991a5
topister <topistern2@gmail.com>
2024-04-13 18:08:32 +0200

    deployment

--- emarketproject/emarketproject/settings.py
+++ emarketproject/emarketproject/settings.py
@@ -63,7 +63,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,13 +141,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-  
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'  
 
 customColorPalette = [
         {
